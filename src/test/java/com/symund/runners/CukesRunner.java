10ba12a5d2f0151725be4a6d35cbde08afca52f7
ekintb <ekint17@gmail.com>
2022-02-23 23:00:58 +0300

    verified

--- src/test/java/com/symund/runners/CukesRunner.java
+++ src/test/java/com/symund/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/symund/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@logout"
 )
 
 public class CukesRunner {
